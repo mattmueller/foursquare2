b63a574c4af04256c5b0ebb6b428dc636aca3f10
Matt Mueller <muellermr@gmail.com>
2011-02-10 09:24:01 -0600

    Completing venue methods

--- lib/foursquare2/venues.rb
+++ lib/foursquare2/venues.rb
@@ -6,5 +6,44 @@ module Foursquare2
       return_error_or_body(response, response.body.response.venue)
     end
 
+    def search_venues(options={})
+      response = connection.get do |req|
+        req.url "venues/search", options
+      end
+      return_error_or_body(response, response.body.response)
+    end
+
+    def venue_categories
+      response = connection.get("venues/categories")
+      return_error_or_body(response, response.body.response.categories)
+    end
+
+    def add_venue(options={})
+      response = connection.post do |req|
+        req.url "venues/add", options
+      end
+      return_error_or_body(response, response.body.response.venue)
+    end
+
+    def mark_venue_todo(venue_id, options={})
+      response = connection.post do |req|
+        req.url "venues/#{venue_id}/marktodo", options
+      end
+      return_error_or_body(response, response.body.response)
+    end
+
+    def flag_venue(venue_id, options={})
+      response = connection.post do |req|
+        req.url "venues/#{venue_id}/flag", options
+      end
+      return_error_or_body(response, response.body.response)
+    end
+
+    def propose_venue_edit(venue_id, options={})
+      response = connection.post do |req|
+        req.url "venues/#{venue_id}/proposeedit", options
+      end
+      return_error_or_body(response, response.body.response)
+    end
   end 
 end
