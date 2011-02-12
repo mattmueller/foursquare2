4a9b5e16619ee2eaa58f15ceba7ec657473ce2db
Matt Mueller <muellermr@gmail.com>
2011-02-12 06:56:52 -0600

    Adding checkin documentation

--- lib/foursquare2/checkins.rb
+++ lib/foursquare2/checkins.rb
@@ -1,10 +1,21 @@
 module Foursquare2
   module Checkins
 
+    # Retrive information about a single checkin.
+    #
+    # @param [String] checkin_id the ID of the checkin.
+
     def checkin(checkin_id)
       response = connection.get("checkins/#{checkin_id}")
       return_error_or_body(response, response.body.response.checkin)
     end
+
+    # Retrive a list of recent checkins from friends.
+    #
+    # @param [Hash] options
+    # @option options String :ll - Latitude and longitude in format LAT,LON
+    # @option options String :limit - Maximum results to return (max 100)
+    # @option options Integer :afterTimestamp - Seconds after which to look for checkins
 
     def recent_checkins(options={})
       response = connection.get do |req|
@@ -13,6 +24,18 @@ module Foursquare2
       return_error_or_body(response, response.body.response.recent)
     end
 
+    # Checkin on behalf of the user.
+    #
+    # @param [Hash] options
+    # @option options String :venueId - Venue for the checkin.
+    # @option options String :venue - For venueless checkins (name or other identifier)
+    # @option options String :shout - Message to be included with the checkin.
+    # @option options String :broadcast - Required, one or more of private,public,facebook,twitter.  Comma-separated.
+    # @option options String :ll - Latitude and longitude in format LAT,LON
+    # @option options Integer :llAcc - Accuracy of the lat/lon in meters.
+    # @option options Integer :alt - Altitude in meters
+    # @option options Integer :altAcc - Accuracy of the altitude in meters
+
     def add_checkin(options={})
       response = connection.post do |req|
         req.url "checkins/add", options
@@ -20,12 +43,24 @@ module Foursquare2
       return_error_or_body(response, response.body.response.checkin)
     end
 
+    # Add a comment to a checkin.
+    #
+    # @param [String] checkin_id the ID of the checkin.
+    # @param [Hash]  options
+    # @option options String :text - Text of the comment to add, 200 character max.
+
     def add_checkin_comment(checkin_id, options={})
       response = connection.post do |req|
         req.url "checkins/#{checkin_id}/addcomment", options
       end
       return_error_or_body(response, response.body.response)  
     end
+
+    # Delete a checkin's comment
+    #
+    # @params [String] checkin_id - The ID of the checkin
+    # @param [Hash]  options
+    # @option options String :commentId - ID of the comment to be deleted.
 
     def delete_checkin_comment(checkin_id, options={})
       response = connection.post do |req|
