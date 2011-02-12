8696dee90f31e396b47ecc07d46b2902f5522426
Matt Mueller <muellermr@gmail.com>
2011-02-12 06:57:46 -0600

    Adding photo documentation

--- lib/foursquare2/photos.rb
+++ lib/foursquare2/photos.rb
@@ -1,6 +1,10 @@
 module Foursquare2
   module Photos
 
+    # Retrive a photo
+    #
+    # @params [String] photo_id - The ID of the photo
+
     def photo(photo_id)
       response = connection.get("photos/#{photo_id}")
       return_error_or_body(response, response.body.response.photo)
