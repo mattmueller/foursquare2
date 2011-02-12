91577b40f8f4e164ec9b61abb702e69ee379b54e
Matt Mueller <muellermr@gmail.com>
2011-02-12 07:11:22 -0600

    Adding documentation for tips

--- lib/foursquare2/tips.rb
+++ lib/foursquare2/tips.rb
@@ -1,10 +1,23 @@
 module Foursquare2
   module Tips
 
+    # Retrieve information about a tip.
+    #
+    # param [String] tip_id - The id of the tip to retrieve.
+
     def tip(tip_id)
       response = connection.get("tips/#{tip_id}")
       return_error_or_body(response, response.body.response.tip)
     end    
+
+    # Search for tips.
+    #
+    # @param [Hash]  options
+    # @option options String :ll - Latitude and longitude in format LAT,LON
+    # @option options Integer :limit - The limit of results to return.
+    # @option options Integer :offset - Used to page through results.
+    # @option options String :filter - Set to 'friends' to limit tips to those from friends.
+    # @option options String :query - Only find tips matching this term.
 
     def search_tips(options={})
       response = connection.get do |req|
@@ -13,12 +26,23 @@ module Foursquare2
       return_error_or_body(response, response.body.response.tips)
     end
 
+    # Add a tip
+    #
+    # @param [Hash]  options
+    # @option options String :venueId - Required, which venue to add the tip to.
+    # @option options String :text - The text of the tip.
+    # @option options String :url - Optionally associated url.
+
     def add_tip(options={})
       response = connection.post do |req|
         req.url "tips/add", options
       end
       return_error_or_body(response, response.body.response.tip)
     end
+
+    # Mark a tip todo for the authenticated user.
+    #
+    # param [String] tip_id - The id of the tip to mark.
 
     def mark_tip_todo(tip_id)
       response = connection.post do |req|
@@ -27,12 +51,20 @@ module Foursquare2
       return_error_or_body(response, response.body.response)
     end
 
+    # Mark a tip done for the authenticated user.
+    #
+    # param [String] tip_id - The id of the tip to mark.
+
     def mark_tip_done(tip_id)
       response = connection.post do |req|
         req.url "tips/#{tip_id}/markdone"
       end
       return_error_or_body(response, response.body.response)
     end
+
+    # Unmark a tip todo for the authenticated user.
+    #
+    # param [String] tip_id - The id of the tip to unmark.
 
     def unmark_tip(tip_id)
       response = connection.post do |req|
