a30fe0567a7e1b11356aa61c36688309a2f3e9d4
Matt Mueller <muellermr@gmail.com>
2012-07-08 06:15:09 -0500

    Adding basic tests for new pages functionality.

---
+++ test/test_pages.rb
@@ -0,0 +1,31 @@
+require 'helper'
+
+class TestPages < Test::Unit::TestCase
+
+  context "When using the foursquare API and working with pages" do
+    setup do
+      @client = foursquare_test_client
+    end
+
+    should "fetch info for a single page" do
+      stub_get("https://api.foursquare.com/v2/pages/14046309?oauth_token=#{@client.oauth_token}", "page.json")
+      page = @client.page('14046309')
+      page.firstName.should == "Nashville Geeks"
+      page.type.should == "page"
+    end
+
+    should "fetch results when searching for a page" do
+      stub_get("https://api.foursquare.com/v2/pages/search?name=NashvilleGeeks&oauth_token=#{@client.oauth_token}", "search_pages.json")
+      pages = @client.search_pages(:name => 'NashvilleGeeks')
+      pages.results.first.firstName.should == "Nashville Geeks"
+    end
+
+    should "fetch venues for a given page" do
+      stub_get("https://api.foursquare.com/v2/pages/1070527/venues?limit=2&oauth_token=#{@client.oauth_token}", "page_venues.json")
+      venues = @client.page_venues(1070527, :limit => 2)
+      venues['count'].should == 11051
+      venues.items.length.should == 2
+      venues.items.first.name.should == "Starbucks"
+    end
+  end
+end
