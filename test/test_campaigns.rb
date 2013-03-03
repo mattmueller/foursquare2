587921687bb955400bb9948a48e85a84ea3e1d01
Matt Mueller <muellermr@gmail.com>
2013-03-03 16:25:46 -0600

    Fixing broken specs related to listing campaigns.

--- test/test_campaigns.rb
+++ test/test_campaigns.rb
@@ -1,5 +1,7 @@
 require 'helper'
 
+require 'helper'
+
 class TestCampaigns < Test::Unit::TestCase
 
   context "When using the foursquare API and working with campaigns" do
@@ -14,15 +16,15 @@ class TestCampaigns < Test::Unit::TestCase
     end
 
     should "list all campaigns" do
-      stub_post("https://api.foursquare.com/v2/campaigns/add?oauth_token=#{@client.oauth_token}&specialId=4bd876f886ba62b58a6e88b3", "campaigns/campaigns_list.json")
+      stub_get("https://api.foursquare.com/v2/campaigns/list?oauth_token=#{@client.oauth_token}&status=active", "campaigns/campaigns_list.json")
       campaigns = @client.list_campaigns(:status => 'active')
-      campaigns.first.id == "4e0deba2922e6f94b1410b79"
+      campaigns.first.id.should == "4e0deba2922e6f94b1410b79"
     end
 
     should "list campaigns of specialId" do
-      stub_post("https://api.foursquare.com/v2/campaigns/add?oauth_token=#{@client.oauth_token}&specialId=4bd876f886ba62b58a6e88b3", "campaigns/campaigns_list.json")
+      stub_get("https://api.foursquare.com/v2/campaigns/list?oauth_token=#{@client.oauth_token}&specialId=4e0debea922e6f94b1410bb7", "campaigns/campaigns_list.json")
       campaigns = @client.list_campaigns(:specialId => '4e0debea922e6f94b1410bb7')
-      campaigns.special.id == "4e0debea922e6f94b1410bb7"
+      campaigns.first['specialId'].should == "4e0debea922e6f94b1410bb7"
     end
 
   end
