d7b990655f26c9b1c6091a5952a27391c61f2ee3
Alex Meyer <acmeyer9@gmail.com>
2013-04-04 18:17:05 -0400

    updated

--- lib/foursquare2/venuegroups.rb
+++ lib/foursquare2/venuegroups.rb
@@ -5,8 +5,19 @@ module Foursquare2
     #
     # param [String] venuegroup_id The ID of the venue
 
-    def venue(venuegroup_id)
+    def venuegroup(venuegroup_id)
       response = connection.get("venuegroups/#{venuegroup_id}")
+      return_error_or_body(response, response.body.response.venuegroup)
+    end
+
+    # Add a venuegroup
+    # @option options String :name - Required
+    # @option options String :venueId - If specified, a comma-delimited list of venue IDs that will become the new set of venue IDs for the group.
+
+    def add_venuegroup(options={})
+      response = connection.post do |req|
+        req.url "venuegroups/add", options
+      end
       return_error_or_body(response, response.body.response.venuegroup)
     end
 
--- test/test_venuegroups.rb
+++ test/test_venuegroups.rb
@@ -13,6 +13,12 @@ class TestVenuegroups < Test::Unit::TestCase
       venuegroup.id == "4e15cd13b61c42e7c54e5bb6"
     end
 
+    should "add a venuegroup" do
+      stub_post("https://api.foursquare.com/v2/venuegroups/add?oauth_token=#{@client.oauth_token}?name=Venues&venueId=4b8c3d87f964a520f7c532e3", "venuegroups/venuegroup.json")
+      venuegroup = @client.add_venuegroup()
+      venuegroup.name == "Venue"
+    end
+
     should "update a venuegroup" do
       stub_post("https://api.foursquare.com/v2/venuegroups/4e15cd13b61c42e7c54e5bb6/update?oauth_token=#{@client.oauth_token}?venueId=4b8c3d87f964a520f7c532e3,4d572bcc9e508cfab975189b", "venuegroups/venuegroup_update.json")
       venuegroup = @client.venuegroup('4e15cd13b61c42e7c54e5bb6')
