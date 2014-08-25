f93f990b1667406607548faa14a3955fc75ff3ed
Leandro Gualter <leandrogualter@gmail.com>
2014-08-25 14:00:07 -0300

    Upgrades mocha version for ruby 2.1 compatibility

--- test/helper.rb
+++ test/helper.rb
@@ -14,7 +14,7 @@ require 'fakeweb'
 require 'json'
 require 'hashie'
 require 'awesome_print'
-require 'mocha'
+require 'mocha/mini_test'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
