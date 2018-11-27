15dbd6d8bd67e8f3e41a3c8d5d8064b9d3863547
Gerardo Ortega <geraldavid7@gmail.com>
2018-11-27 17:33:51 -0600

    Updagrading libraries, applying Rubocop recommendations

--- lib/foursquare2.rb
+++ lib/foursquare2.rb
@@ -1,47 +1,40 @@
-require 'faraday'
-require 'faraday_middleware'
+require "faraday"
+require "faraday_middleware"
 
-directory = File.expand_path(File.dirname(__FILE__))
+__dir__
 
 module Foursquare2
   class << self
-
-    FIELDS = [ :client_id, :client_secret, :api_version,
-               :ssl, :connection_middleware, :locale ]
+    FIELDS = [:client_id, :client_secret, :api_version, :ssl, :connection_middleware, :locale].freeze
     attr_accessor(*FIELDS)
 
-    def filter tips, term
+    def filter(tips, term)
       tip = []
-      unless tips.nil?
-        tips.items.each do |check_tip|
-          tip << check_tip if check_tip.text.downcase.include? term.downcase
-        end
+      tips&.items&.each do |check_tip|
+        tip << check_tip if check_tip.text.downcase.include? term.downcase
       end
-      Hashie::Mash.new({:count => tip.count, :items => tip })
+      HashWrapper.new(count: tip.count, items: tip)
     end
 
     def configure
       yield self
       true
     end
-
   end
 
-  require 'foursquare2/campaigns'
-  require 'foursquare2/users'
-  require 'foursquare2/specials'
-  require 'foursquare2/settings'
-  require 'foursquare2/photos'
-  require 'foursquare2/tips'
-  require 'foursquare2/checkins'
-  require 'foursquare2/venues'
-  require 'foursquare2/venuegroups'
-  require 'foursquare2/pages'
-  require 'foursquare2/lists'
-  require 'foursquare2/events'
-  require 'foursquare2/client'
-  require 'foursquare2/api_error'
-
-
+  require "foursquare2/hash_wrapper"
+  require "foursquare2/campaigns"
+  require "foursquare2/users"
+  require "foursquare2/specials"
+  require "foursquare2/settings"
+  require "foursquare2/photos"
+  require "foursquare2/tips"
+  require "foursquare2/checkins"
+  require "foursquare2/venues"
+  require "foursquare2/venuegroups"
+  require "foursquare2/pages"
+  require "foursquare2/lists"
+  require "foursquare2/events"
+  require "foursquare2/client"
+  require "foursquare2/api_error"
 end
-
